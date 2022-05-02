1f55a6256f22c1f8c10e42844e7e6836ad64887d
Corey <1609495628@qq.com>
2022-05-02 17:33:17 +0800

    init

---
+++ src/main/java/com/example/nettysutdy/NettySutdyApplication.java
@@ -0,0 +1,13 @@
+package com.example.nettysutdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySutdyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySutdyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nettysutdy/NettySutdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nettysutdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettySutdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
